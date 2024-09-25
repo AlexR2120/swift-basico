c17ffdef1b8e166d58e927f3eba88d3f4e82fce2
Alex Alumno Mac <alejandro_ruiz_m-tsapp1ma2324@cev.com>
2024-09-25 09:11:01 +0100

    Add Special Characters function

--- swift-basico/main.swift
+++ swift-basico/main.swift
@@ -1,11 +1,42 @@
-//
-//  main.swift
-//  swift-basico
-//
-//  Created by user231456 on 19/09/2024.
-//
+// import Foundation
 
-import Foundation
+// print("888 PokerStars")
 
-print("888 PokerStars")
+func tieneCaracteresEspeciales(texto: String) -> Bool {
+    let especiales = "!¡$%/()?¿+-*,.;:{}[]"
+    for letra in texto {
+        for especial in especiales {
+            if letra == especial {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+print("¿Cómo te llamas?")
+var nombre = readLine()!
+var nombreIncorrecto = false
+
+repeat {
+    if nombre.count > 10 { // Máximo 10 letras
+        print("ERROR!: Nombre demasiado largo, escribe otro")
+        nombreIncorrecto = true
+        nombre = readLine()!
+    } else if nombre.first!.isNumber { // No empezar por número
+        nombreIncorrecto = true
+        print("ERROR!: No se puede empezar con un número, escribe otro")
+        nombre = readLine()!
+    } else if
+        tieneCaracteresEspeciales(texto: <#T##String#>) { // Sin carácteres especiales
+        nombreIncorrecto = true
+        print("ERROR! No se puede empezar con un número, escribe otro nombre")
+        nombre = readLine()!
+    } else {
+        nombreIncorrecto = false
+    }
+} while nombreIncorrecto
+
+print("Hola", nombre)
+
 

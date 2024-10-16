0efeebde0eb59d60fc82768b3ae7fdff84cc9a36
Alex Alumno Mac <alejandro_ruiz_m-tsapp1ma2324@cev.com>
2024-10-16 12:51:14 +0100

    Ejercicio 6 y 7

--- swift-basico/main.swift
+++ swift-basico/main.swift
@@ -152,7 +152,7 @@ else {
 */
 
 // EJERCICIO 4
-
+/*
 print("Introduce una frase: ")
 if let frase = readLine()
 {
@@ -182,10 +182,159 @@ if let frase = readLine()
     
     print(resultado)
 }
+*/
 
+// EJERCICIO 5
+/*
+print("Listado de números romanos:\n",
+      "I = 1\n",
+      "V = 5\n",
+      "X = 10\n",
+      "L = 50\n",
+      "C = 100\n",
+      "D = 500\n",
+      "M = 1000\n")
 
+func romanoADecimal(_ romano: String) -> Int
+{
+    // Diccionario de los valores de los números romanos
+    let valoresRomanos: [Character : Int] =
+    [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
+        
+    var resultado = 0
+    
+    for letra in romano
+    {
+        if let valor = valoresRomanos[letra]
+        {
+            resultado += valor
+        }
+    }
+    return resultado
+}
 
+print("Introduzca un número romano:")
 
+if let numeroRomano = readLine()
+{
+    let numeroDecimal = romanoADecimal(numeroRomano.uppercased())
+    
+    print("\(numeroRomano) = \(numeroDecimal)")
+}
+*/
+// EJERCICIO 6
+/*
+print("Listado de números romanos:\n",
+      "I = 1\n",
+      "V = 5\n",
+      "X = 10\n",
+      "L = 50\n",
+      "C = 100\n",
+      "D = 500\n",
+      "M = 1000\n")
 
+func romanoADecimal(_ romano: String) -> Int
+{
+    // Diccionario de los valores de los números romanos
+    let valoresRomanos: [Character : Int] =
+    [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
+        
+    var resultado = 0
+    var valorPrevio = 1
+    
+    for letra in romano
+    {
+        if let valor = valoresRomanos[letra]
+        {
+            if valorPrevio < valor
+            {
+                resultado += valor
+            }
+            else
+            {
+                resultado -= valor
+            }
+            valorPrevio = valor
+            
+        }
+    }
+    return resultado
+}
 
+print("Introduzca un número romano:")
+
+if let numeroRomano = readLine()
+{
+    let numeroDecimal = romanoADecimal(numeroRomano.uppercased())
+    
+    print("\(numeroRomano) = \(numeroDecimal)")
+}
+*/
+// EJERCICIO 7
+/*
+// Array con números integrados
+var num : [Int] = [3,5,8,32,17]
+
+// Sumamos los elementos del Array
+var arraySum = num.reduce(0, +)
+
+// Calculamos el total de
+// elementos del Array
+var length = num.count
+
+// Sacamos la media
+var media = Double(arraySum)/Double(length)
+
+print(num)
+print("La media es: ", media)
+*/
+
+/*
+print("Introduce una frase: ")
+if let frase = readLine()
+{
+    // Separa las palabras de la frase
+    let palabras = frase.split(separator: " ").map { String($0) }
+    
+    // Crear Array para las palabras censuradas
+    var palabrasCensuradas = [String]()
+    
+    // Recorrer cada palabra
+    for palabra in palabras {
+        // Comprobar si empieza por "j", "p" o "m" (minúsculas o mayúsculas)
+        if palabra.lowercased().hasPrefix("j") || palabra.lowercased().hasPrefix("p")
+            || palabra.lowercased().hasPrefix("m") {
+            
+            // Añadirle asteríscos a las palabras censuradas
+            palabrasCensuradas.append("*****")
+        }
+        else {
+            // Si no, agregar la frase sin censura.
+            palabrasCensuradas.append(String(palabra))
+        }
+    }
+    
+    // Crear la frase
+    let resultado = palabrasCensuradas.joined(separator: " ")
+    
+    print(resultado)
+}
+*/
+// EJERCICIO 8
 
